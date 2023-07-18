d5a66385b27630405f1d0a6d947971b0a7961756
Tamene Gelaye <tamenes21@gmail.com>
2023-07-18 07:39:06 -0700

    search a school

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Printing a list of schools to learn python'''
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''A function which return a list of schools'''
+    mongo_collection.find({"topics: {"$in":[topics]}})
