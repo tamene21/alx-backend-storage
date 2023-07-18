1d89d8ab7e42e726e3bcbcf7f8697b9813178955
Tamene Gelaye <tamenes21@gmail.com>
2023-07-18 07:06:51 -0700

    update topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''A script to change a name'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''A function to update a topic'''
+    return mongo_collection({
+            "name": name
+            },
+            {
+                "$set": {
+                    "topics": topics
+                    }
+                })
