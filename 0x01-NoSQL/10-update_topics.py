6cd752cbad154fb336863b7164ad51600c36db3e
Tamene Gelaye <tamenes21@gmail.com>
2023-07-18 07:23:41 -0700

    update syntax

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,7 +5,7 @@ import pymongo
 
 def update_topics(mongo_collection, name, topics):
     '''A function to update a topic'''
-    return mongo_collection({
+    return mongo_collection.update_many({
             "name": name
             },
             {
