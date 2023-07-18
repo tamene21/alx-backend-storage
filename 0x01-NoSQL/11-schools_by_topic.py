39c68c1e08613a187492fce15753ac57454220c9
Tamene Gelaye <tamenes21@gmail.com>
2023-07-18 08:12:00 -0700

    edited

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,4 +5,4 @@ import pymongo
 
 def schools_by_topic(mongo_collection, topic):
     '''A function which return a list of schools'''
-    return mongo_collection.find({"topic": {"$in": [topics]}})
+    return mongo_collection.find({"topics": {"$in": [topic]}})
